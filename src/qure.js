08c48ace343f5a5f8007d57479e28cfcf4e68a46
Hakan Bilgin <hbi99@hotmail.com>
2015-11-22 22:47:54 +0100

    refactoring qure source

--- src/qure.js
+++ src/qure.js
@@ -357,39 +357,7 @@
 
 	if (isNode) {
 		// worker class for node environment
-		var NodeWorker = function() {
-			var that = this,
-				ps   = require('child_process');
-			// fork child process
-			this.process = ps.fork(__dirname +'/eval');
-
-			// prepare out-bound com
-			this.process.on('message', function (msg) {
-				that.terminate();
-
-				if (that.onmessage) {
-					that.onmessage({ data: JSON.parse(msg) });
-				}
-			});
-			
-			// error handler (todo)
-			this.process.on('error', function (err) {
-				if (that.onerror) {
-					that.onerror(err);
-				}
-			});
-		};
-
-		NodeWorker.prototype = {
-			onmessage: null,
-			onerror: null,
-			postMessage: function (obj) {
-				this.process.send(JSON.stringify({ data: obj }));
-			},
-			terminate: function () {
-				//this.process.kill();
-			}
-		};
+		var NodeWorker = require('./worker');
 	}
 
 	// Export
