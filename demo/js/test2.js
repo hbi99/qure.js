1f2fb8651cd63eb1a30456e718bd4b09686edb2a
Hakan Bilgin <hbi99@hotmail.com>
2015-12-25 19:56:49 +0100

    extending test javascript file

--- demo/js/test2.js
+++ demo/js/test2.js
@@ -2,10 +2,16 @@
 console.log('External javascript loaded #2');
 
 var test = function() {
-	console.log(this);
+	
 };
 
 test.prototype = {
+	// declare type (omitted or false = not threaded)
+	workers: true,
+	// fibonacci numbers
+	fibonacci: function(n) {
+		return (n < 2) ? 1 : self(n-2) + self(n-1);
+	},
 	foo: function() {
 		return 'bar';
 	}
