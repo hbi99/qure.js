12bc9f15fee80540254387a718b3f62864461264
Hakan Bilgin <hbi99@hotmail.com>
2016-01-21 10:50:45 +0100

    removing test script

--- src/qure.js
+++ src/qure.js
@@ -510,32 +510,6 @@
 		// worker class for node environment
 		window.Worker = require('./worker');
 		window.XMLHttpRequest = require('./xhr');
-	} else {
-		// auxiliary methods
-		window.xNode = function() {};
-		xNode.selectNodes = function(XNode, XPath) {
-			if (XNode.evaluate) {
-				var ns = XNode.createNSResolver(XNode.documentElement),
-					qI = XNode.evaluate(XPath, XNode, ns, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null),
-					res = [],
-					i   = 0,
-					il  = qI.snapshotLength;
-				for (; i<il; i++) {
-					res.push( qI.snapshotItem(i) );
-				}
-				return res;
-			} else {
-				return XNode.selectNodes(XPath);
-			}
-		};
-		xNode.selectSingleNode = function(XNode, XPath) {
-			if (XNode.evaluate) {
-				var xI = this.selectNodes(XNode, XPath);
-				return (xI.length > 0)? xI[0] : null;
-			} else {
-				return XNode.selectSingleNode(XPath);
-			}
-		};
 	}
 
 	// Export
