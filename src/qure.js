15d3718ab0819c977bad92b93c94e23ac6db7c7a
Hakan Bilgin <hbi99@hotmail.com>
2016-01-21 09:43:28 +0100

    first version of xslt demo

--- src/qure.js
+++ src/qure.js
@@ -256,10 +256,15 @@
 						ret = parser.parseFromString(str, "text/html");
 					}
 					break;
-			//	case 'xml':
-			//		break;
-			//	case 'xsl':
-			//		break;
+				case 'xml':
+				case 'xsl':
+					if (isNode) {
+						ret = str;
+					} else {
+						parser = new DOMParser();
+						ret = parser.parseFromString(str, "text/xml");
+					}
+					break;
 				default:
 					ret = str;
 			}
@@ -506,6 +511,32 @@
 		// worker class for node environment
 		window.Worker = require('./worker');
 		window.XMLHttpRequest = require('./xhr');
+	} else {
+		// auxiliary methods
+		window.xNode = function() {};
+		xNode.selectNodes = function(XNode, XPath) {
+			if (XNode.evaluate) {
+				var ns = XNode.createNSResolver(XNode.documentElement),
+					qI = XNode.evaluate(XPath, XNode, ns, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null),
+					res = [],
+					i   = 0,
+					il  = qI.snapshotLength;
+				for (; i<il; i++) {
+					res.push( qI.snapshotItem(i) );
+				}
+				return res;
+			} else {
+				return XNode.selectNodes(XPath);
+			}
+		};
+		xNode.selectSingleNode = function(XNode, XPath) {
+			if (XNode.evaluate) {
+				var xI = this.selectNodes(XNode, XPath);
+				return (xI.length > 0)? xI[0] : null;
+			} else {
+				return XNode.selectSingleNode(XPath);
+			}
+		};
 	}
 
 	// Export
