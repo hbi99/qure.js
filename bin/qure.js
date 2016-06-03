f457ff62f7ec6e908317aac4dd6f8fe5e83021ee
Hakan Bilgin <hbi99@hotmail.com>
2016-06-03 16:54:37 +0200

    testing

---
+++ bin/qure.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+console.log('Hello, world!');
+//require('../lib/cli').run(process.argv.slice(2));
---
+++ tests/test-16.js
@@ -0,0 +1,28 @@
+
+/*
+ * 
+ */
+
+var Qure = require('../src/qure.js');
+
+describe('Loading HTML file', function() {
+
+	/* 
+	 * 
+	 */
+	it('on file system', function(done) {
+		
+		Qure
+			.load(__dirname +'/../demo/html/index.htm')
+			.then(function(res) {
+				console.log(res);
+			//	if (res.foo() !== 'bar') {
+			//		console.log( '\tUnexpected value!' );
+			//	}
+				done();
+			});
+		
+	});
+
+});
+
